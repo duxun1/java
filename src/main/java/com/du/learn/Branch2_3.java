6dc8afd8c18ed5bab71f6181e1e63ab4ab3c1e5c
duxun <dlx4339@163.com>
2022-05-03 17:15:16 +0800

    Branch2分支修改Branch3.java 创建Branch2_3.java

--- src/main/java/com/du/learn/Branch2_3.java
+++ src/main/java/com/du/learn/Branch2_3.java
@@ -1,7 +1,8 @@
 package com.du.learn;
 
-public class Branch3 {
+public class Branch2_3 {
     public static void main(String[] args) {
-        System.out.println("Branch2.2");
+        System.out.println("Branch2_3");
     }
 }
+
