81f4b2fceb91432c49500c5cf8e29d443b0878d0
duxun <dlx4339@163.com>
2022-05-03 16:35:07 +0800

    master修改了Hello.java文件

--- src/main/java/com/du/learn/Hello.java
+++ src/main/java/com/du/learn/Hello.java
@@ -2,7 +2,7 @@ package com.du.learn;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello,world");
+        System.out.println("hello,world master");
     }
 
 
